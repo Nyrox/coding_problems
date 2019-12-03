300d85a26c764b521f56bd68ed5c84116c14396e
Nyrox <root@nyrox.tech>
2019-12-03 13:43:03 +0100

    Implements Part One of Day Two of AOC19

---
+++ advent_of_code_2019/02/main.hs
@@ -0,0 +1,71 @@
+import System.IO
+import Data.List
+import Data.Tuple
+import Data.Maybe
+
+wordsWhen :: (Char -> Bool) -> String -> [String]
+wordsWhen p s =  case dropWhile p s of
+                      "" -> []
+                      s' -> w : wordsWhen p s''
+                            where (w, s'') = break p s'
+
+type Program = [(Integer, Integer)]
+
+insertCell :: Program -> Integer -> Integer -> Program
+insertCell program p v =
+    (p, v) : program
+
+initCells :: [Integer] -> Integer -> Program
+initCells [] offset = []
+initCells values offset =
+    (offset, head values) : (initCells (tail values) (offset + 1))
+
+initProgram :: String -> Program
+initProgram input =
+    let cells = [read x :: Integer | x <- wordsWhen (==',') input]
+    in
+        initCells cells 0
+
+readCell :: Program -> Integer -> Integer
+readCell program offset =
+    let cell = find (\t -> (fst t) == offset) program
+    in
+        snd (fromJust cell)
+
+writeCell :: Program -> Integer -> Integer -> Program
+writeCell program address value =
+    (address, value) : program
+
+step :: Program -> Integer -> (Bool, Program, Integer)
+step program isp =
+    let opcode = readCell program isp
+    in if opcode == 99
+        then (True, program, 0)
+        else
+            let lhs = readCell program (readCell program (isp + 1))
+                rhs = readCell program (readCell program (isp + 2))
+                out = readCell program (isp + 3)
+            in
+                if opcode == 1
+                    then (False, writeCell program out (lhs + rhs), isp + 4)
+                else if opcode == 2
+                    then (False, writeCell program out (lhs * rhs), isp + 4)
+                else error "Cursed state reached"
+
+runToEnd program isp =
+    let (finished, nextProgram, nextIsp) = step program isp
+    in
+        if finished then nextProgram
+        else runToEnd nextProgram nextIsp
+
+main = do
+    inputFile <- openFile "input" ReadMode
+    input <- hGetContents inputFile
+
+    let p1 = initProgram input
+    let p2 = writeCell p1 1 12
+    let p3 = writeCell p2 2 2
+
+    putStrLn (show (runToEnd p3 0))
+
+
